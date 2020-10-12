5bfe34196999cc3fcfd42d4033d969156f925279
w100672 <w100672@192.168.1.9>
2020-10-12 12:56:54 +0530

    second commit

--- NoConversion/src/NoConversion.cpp
+++ NoConversion/src/NoConversion.cpp
@@ -9,7 +9,97 @@
 #include <iostream>
 using namespace std;
 
+class Number
+{
+protected:
+	int value;
+
+public:
+	Number(int v)
+	{
+		value=v;
+	}
+	virtual int print_it()
+	{
+		return value;
+	}
+};
+
+class Hex : public Number
+{
+public:
+	Hex(int n)
+	{
+		Number(n);
+	}
+
+	int print_it()
+		{
+			int n= print_it();
+			char hex[100];
+			int i=0;
+			while(n!=0)
+			{
+				//temp for remainder
+				int temp = 0;
+
+				temp = n % 16;
+
+				if(temp < 10)
+				{
+					hex[i]= temp + 48;
+					i++;
+				}
+				else
+				{
+					hex[i] = temp + 55;
+					i++;
+				}
+
+				n=n/16;
+			}
+
+			//Printing in reverse order
+			for(int j=i-1; j>=0;j--)
+			{
+				cout<<hex[j];
+			}
+		}
+
+};
+
+class Octal : public Number
+{
+public:
+	int print_it()
+	{
+		int n = print_it();
+		int oct[100];
+
+		int i=0;
+		while(n!=0)
+		{
+			oct[i]= n % 8;
+			n= n/8;
+			i++;
+		}
+
+		//print in reverse order
+		for(int j=i-1; j>=0; j--)
+		{
+			cout<<oct[j];
+		}
+
+	}
+
+};
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int number;
+	cout<<"Enter an Integer : ";
+	cin>>number;
+
+	Number *n1 =new  Hex(number);
+	n1->print_it();
 	return 0;
 }
